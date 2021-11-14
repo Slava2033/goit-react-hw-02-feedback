6d709cd70fa5b7ceb9cd78a87d0eecd726995d86
Slava2033 <Godlikefortune@gmail.com>
2021-11-14 18:47:53 +0200

    Signed-off-by: Slava2033 <Godlikefortune@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Section from './components/Section/section';
+import Statistics from './components/Statistics/statistics';
+import FeedbackOptions from './components/FeedbackOptions/feedbackOptions';
+
+const options = ['good', 'bad', 'neutral'];
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + bad + neutral;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    let totalFeedback = this.countTotalFeedback();
+    const { good } = this.state;
+    return totalFeedback ? Math.round((good / totalFeedback) * 100) : 0;
+  };
+
+  handleFeedback = ({ target }) => {
+    const { feedback } = target.dataset;
+    this.setState(prevState => {
+      return { [feedback]: prevState[feedback] + 1 };
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedbackCount = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage();
+
+    return (
+      <>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+        <Section title={'Statistics'}>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedbackCount}
+            positiveFeedbackPercentage={positiveFeedback}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
